91b78704dbf5b4519ec9da6cb4fe0f0b4192e8f3
randomaru <m.duval@yandex.ru>
2020-04-15 17:22:15 +0300

    add test.c

--- test.c
+++ test.c
@@ -1,4 +1,37 @@
-//
-// Created by mac on 4/15/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamarant <tamarant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/15 15:43:26 by mac               #+#    #+#             */
+/*   Updated: 2020/04/15 16:43:46 by mac              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "inc/ft_printf.h"
+
+int 	main(int argc, char **argv)
+{
+	int orig = 0;
+	int my = 0;
+	char *str = "The fish listened intently to what the frogs had to say.";
+
+	ft_printf(LBL"Hello! It's my ft_printf!\nLet's test some basic features:\n"RST);
+	ft_printf( YEL"String = \"%s\"\nwith field precision == 17 and newline in the end.\n"RST, str);
+	ft_printf(RED"---Original printf---\n"RST);
+	ft_printf("The number of printed characters: %i\n", orig = printf("%.17s\n", str));
+	ft_printf(PPL"---ft_printf---\n"RST);
+	ft_printf("The number of printed characters: %i\n", my = ft_printf("%.17s\n", str));
+	if (orig == my)
+		ft_printf(BLU"Congrats! ft_printf repeats the behavior of the original function! \n"RST);
+	ft_printf("Let's try ft_printf with some ints\nlong int = %li\nminimum field width = 30, flag = 0\n", LONG_MAX);
+	ft_printf(RED"---Original printf---\n"RST);
+	ft_printf("The number of printed characters: %i\n", orig = printf("%030li\n", LONG_MAX));
+	ft_printf(PPL"---ft_printf---\n"RST);
+	ft_printf("The number of printed characters: %i\n", my = ft_printf("%030li\n", LONG_MAX));
+	if (orig == my)
+		ft_printf(BLU"Congrats! ft_printf repeats the behavior of the original function! \n"RST);
+	return (0);
+}
